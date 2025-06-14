028e6d8bf2c300686fb1e24fb857a5f0ed59fb28
Sarim <sarim.xs@gmail.com>
2025-06-14 11:26:15 -0600

    Add route

---
+++ src/routes/random-chapter.ts
@@ -0,0 +1,38 @@
+import { WRoute } from "../types/w-route";
+import { WResult } from "../types/w-result";
+import { Quran } from "../data/data-quran";
+
+export default function route(): WRoute {
+    return {
+        url: "/random-chapter",
+        method: "GET",
+        handler: async (req, res) => {
+            const randomChapterInt = Math.floor(Math.random() * (114 - 1 + 1) + 1);
+
+            const result =
+              Quran.data.length > 0
+                ? Quran.data.filter((i) => i.chapter_number === randomChapterInt)
+                : [];
+
+            res.code(result ? 200 : 404).send({
+                message: result ? `Found random chapter: ${result[0].chapter_number}` : "No random chapter found",
+                request: {
+                    type: "chapter",
+                    raw_query: "random-chapter",
+                    parsed_query: {
+                        chapter: randomChapterInt,
+                    },
+                    parsed_options: {},
+                    standard_url: "/random-chapter",
+                },
+                response: {
+                    data: result || [],
+                    copyright: {
+                        text: "Dr. Rashad Khalifa, Ph.D.",
+                        url: "https://masjidtucson.org/"
+                    }
+                },
+            } as WResult);
+        },
+    };
+}
