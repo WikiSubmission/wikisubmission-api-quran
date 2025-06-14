9e2d290eb47f203169e4e3dd7cd2c294b100bb78
Sarim <sarim.xs@gmail.com>
2025-06-14 11:30:32 -0600

    Remove imports

--- src/data/data-quran-word-by-word.ts
+++ src/data/data-quran-word-by-word.ts
@@ -1,7 +1,5 @@
 import { WData } from ".";
 import { Database } from "../types/generated/database.types";
-import fs from "fs";
-import path from "path";
 
 export const QuranWordByWord = new WData<
   Database["public"]["Tables"]["ws-quran-word-by-word"]["Row"][]
