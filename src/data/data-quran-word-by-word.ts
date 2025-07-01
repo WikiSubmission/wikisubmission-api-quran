18bd4c59fd2374a926451c06bbf5737dd29c5520
Sarim <sarim.xs@gmail.com>
2025-06-30 22:04:56 -0400

    Add refresh on change for word by word

--- src/data/data-quran-word-by-word.ts
+++ src/data/data-quran-word-by-word.ts
@@ -3,4 +3,8 @@ import { Database } from "../types/generated/database.types";
 
 export const QuranWordByWord = new WData<
   Database["public"]["Tables"]["ws-quran-word-by-word"]["Row"][]
->("ws-quran-word-by-word");
+>("ws-quran-word-by-word", {
+  gracefullyRefreshOnChanges: { 
+    minutesBeforeResync: 1
+  }
+});
