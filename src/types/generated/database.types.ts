199ca1d3982a6f15576b10894a7c026b59f7d3f3
Sarim <sarimyx@proton.me>
2025-09-10 22:05:07 -0600

    update types

--- src/types/generated/database.types.ts
+++ src/types/generated/database.types.ts
@@ -7,6 +7,11 @@ export type Json =
   | Json[]
 
 export type Database = {
+  // Allows to automatically instantiate createClient with right options
+  // instead of createClient<Database, { PostgrestVersion: 'XX' }>(URL, KEY)
+  __InternalSupabase: {
+    PostgrestVersion: "12.0.2 (a4e00ff)"
+  }
   public: {
     Tables: {
       DataMocMedia: {
@@ -391,6 +396,7 @@ export type Database = {
           chapter_title_arabic: string
           chapter_title_english: string
           chapter_title_transliterated: string
+          chapter_verses: number | null
           global_index: number
           verse_footnote_english: string | null
           verse_id: string
@@ -409,6 +415,7 @@ export type Database = {
           chapter_title_arabic: string
           chapter_title_english: string
           chapter_title_transliterated: string
+          chapter_verses?: number | null
           global_index?: number
           verse_footnote_english?: string | null
           verse_id: string
@@ -427,6 +434,7 @@ export type Database = {
           chapter_title_arabic?: string
           chapter_title_english?: string
           chapter_title_transliterated?: string
+          chapter_verses?: number | null
           global_index?: number
           verse_footnote_english?: string | null
           verse_id?: string
@@ -444,27 +452,27 @@ export type Database = {
       "ws-quran-chapters": {
         Row: {
           chapter_number: number
-          chapter_revelation_order: string
+          chapter_revelation_order: number
           chapter_title_arabic: string
           chapter_title_english: string
           chapter_title_transliterated: string
-          chapter_verses: string
+          chapter_verses: number
         }
         Insert: {
           chapter_number?: number
-          chapter_revelation_order: string
+          chapter_revelation_order: number
           chapter_title_arabic: string
           chapter_title_english: string
           chapter_title_transliterated: string
-          chapter_verses: string
+          chapter_verses: number
         }
         Update: {
           chapter_number?: number
-          chapter_revelation_order?: string
+          chapter_revelation_order?: number
           chapter_title_arabic?: string
           chapter_title_english?: string
           chapter_title_transliterated?: string
-          chapter_verses?: string
+          chapter_verses?: number
         }
         Relationships: []
       }
@@ -578,6 +586,42 @@ export type Database = {
       }
       "ws-quran-word-by-word": {
         Row: {
+          arabic_text: string | null
+          chapter_index: number | null
+          english_text: string | null
+          global_index: number
+          root_word: string | null
+          transliterated_text: string | null
+          verse_id: string | null
+          verse_index: number | null
+          word_index: number | null
+        }
+        Insert: {
+          arabic_text?: string | null
+          chapter_index?: number | null
+          english_text?: string | null
+          global_index: number
+          root_word?: string | null
+          transliterated_text?: string | null
+          verse_id?: string | null
+          verse_index?: number | null
+          word_index?: number | null
+        }
+        Update: {
+          arabic_text?: string | null
+          chapter_index?: number | null
+          english_text?: string | null
+          global_index?: number
+          root_word?: string | null
+          transliterated_text?: string | null
+          verse_id?: string | null
+          verse_index?: number | null
+          word_index?: number | null
+        }
+        Relationships: []
+      }
+      "ws-quran-word-by-word_backup": {
+        Row: {
           arabic_text: string
           english_text: string
           global_index: number
@@ -603,6 +647,36 @@ export type Database = {
           transliterated_text?: string
           verse_id?: string
           word_index?: number
+        }
+        Relationships: []
+      }
+      "ws-quran-word-by-word-old": {
+        Row: {
+          arabic_text: string | null
+          english_text: string | null
+          global_index: number
+          root_word: string | null
+          transliterated_text: string | null
+          verse_id: string | null
+          word_index: number | null
+        }
+        Insert: {
+          arabic_text?: string | null
+          english_text?: string | null
+          global_index: number
+          root_word?: string | null
+          transliterated_text?: string | null
+          verse_id?: string | null
+          word_index?: number | null
+        }
+        Update: {
+          arabic_text?: string | null
+          english_text?: string | null
+          global_index?: number
+          root_word?: string | null
+          transliterated_text?: string | null
+          verse_id?: string | null
+          word_index?: number | null
         }
         Relationships: []
       }
@@ -646,21 +720,25 @@ export type Database = {
   }
 }
 
-type DefaultSchema = Database[Extract<keyof Database, "public">]
+type DatabaseWithoutInternals = Omit<Database, "__InternalSupabase">
+
+type DefaultSchema = DatabaseWithoutInternals[Extract<keyof Database, "public">]
 
 export type Tables<
   DefaultSchemaTableNameOrOptions extends
     | keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
-    | { schema: keyof Database },
+    | { schema: keyof DatabaseWithoutInternals },
   TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof Database
+    schema: keyof DatabaseWithoutInternals
   }
-    ? keyof (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
-        Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
+    ? keyof (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
+        DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
     : never = never,
-> = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
-  ? (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
-      Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+> = DefaultSchemaTableNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals
+}
+  ? (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
+      DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
       Row: infer R
     }
     ? R
@@ -678,14 +756,16 @@ export type Tables<
 export type TablesInsert<
   DefaultSchemaTableNameOrOptions extends
     | keyof DefaultSchema["Tables"]
-    | { schema: keyof Database },
+    | { schema: keyof DatabaseWithoutInternals },
   TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof Database
+    schema: keyof DatabaseWithoutInternals
   }
-    ? keyof Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
+    ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
     : never = never,
-> = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
-  ? Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+> = DefaultSchemaTableNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals
+}
+  ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
       Insert: infer I
     }
     ? I
@@ -701,14 +781,16 @@ export type TablesInsert<
 export type TablesUpdate<
   DefaultSchemaTableNameOrOptions extends
     | keyof DefaultSchema["Tables"]
-    | { schema: keyof Database },
+    | { schema: keyof DatabaseWithoutInternals },
   TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof Database
+    schema: keyof DatabaseWithoutInternals
   }
-    ? keyof Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
+    ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
     : never = never,
-> = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
-  ? Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+> = DefaultSchemaTableNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals
+}
+  ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
       Update: infer U
     }
     ? U
@@ -724,14 +806,16 @@ export type TablesUpdate<
 export type Enums<
   DefaultSchemaEnumNameOrOptions extends
     | keyof DefaultSchema["Enums"]
-    | { schema: keyof Database },
+    | { schema: keyof DatabaseWithoutInternals },
   EnumName extends DefaultSchemaEnumNameOrOptions extends {
-    schema: keyof Database
+    schema: keyof DatabaseWithoutInternals
   }
-    ? keyof Database[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"]
+    ? keyof DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"]
     : never = never,
-> = DefaultSchemaEnumNameOrOptions extends { schema: keyof Database }
-  ? Database[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"][EnumName]
+> = DefaultSchemaEnumNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals
+}
+  ? DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema["Enums"]
     ? DefaultSchema["Enums"][DefaultSchemaEnumNameOrOptions]
     : never
@@ -739,14 +823,16 @@ export type Enums<
 export type CompositeTypes<
   PublicCompositeTypeNameOrOptions extends
     | keyof DefaultSchema["CompositeTypes"]
-    | { schema: keyof Database },
+    | { schema: keyof DatabaseWithoutInternals },
   CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
-    schema: keyof Database
+    schema: keyof DatabaseWithoutInternals
   }
-    ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
+    ? keyof DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
     : never = never,
-> = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
+> = PublicCompositeTypeNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals
+}
+  ? DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
   : PublicCompositeTypeNameOrOptions extends keyof DefaultSchema["CompositeTypes"]
     ? DefaultSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
     : never
